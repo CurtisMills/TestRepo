621686fe7ad20d37efc28f3781058c3c2b690b61
Curtis Mills <MillsC9@cardiff.ac.uk>
2021-02-11 14:20:23 +0000

    added validation for login pages.

--- blog/forms.py
+++ blog/forms.py
@@ -11,26 +11,24 @@ class RegistrationForm(FlaskForm):
     last_name = StringField('Last Name(s)', validators=[Length(max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$', message='Your password should contain at least one uppercase letter, one lowercase letter and one number.')])])
-    confirm_password=PasswordField('Confirm Password', validators = [
+    confirm_password=PasswordField('Confirm Password', validators= [
                                      DataRequired(), EqualTo('password', message='Your passwords DO NOT match.')])
     submit=SubmitField('Register')
+
+    def validate_user(self, username):
+    user=User.query.filter_by(username = username.data).first()
+    if user is not None:
+        raise ValidationError(
+            'Username already exists. Please choose a different username.')
+
+    def validate_email(self, email):
+    user=User.query.filter_by(email = email.data).first()
+    if user is not None:
+        raise ValidationError(
+            'An account with this email address already exsists.')
 
 
 class LoginForm(FlaskForm):
     email=StringField('Email', validators = [DataRequired(), Email()])
     password=PasswordField('Password', validators = [DataRequired()])
     submit=SubmitField('Login')
-
-
-def validate_user(self, username):
-    user=User.query.filter_by(username = username.data).first()
-    if user is not None:
-        raise ValidationError(
-            'Username already exists. Please choose a different username.')
-
-
-def validate_email(self, email):
-    user=User.query.filter_by(email = email.data).first()
-    if user is not None:
-        raise ValidationError(
-            'An account with this email address already exsists.')
